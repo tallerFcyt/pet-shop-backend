5f2d6df60434c8b6b03371fb345c65af0b0a6215
tallerFcyt <petshop.taller@gmail.com>
2023-05-03 20:08:17 -0300

    Actualizando bd

--- database/db.js
+++ database/db.js
@@ -1,17 +1,17 @@
 const { db } = require('../src/config')
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(db.database, db.user, db.password, {
-  host: db.host,
-  dialect: "postgres",
-});
-
-// const sequelize = new Sequelize("postgres://taller:mMcLezbgC9kK4mDIJSyg2GkPx6CV4dGx@dpg-ch9c57bhp8u0vhbv0pfg-a.oregon-postgres.render.com/petshop_b264", {
+// const sequelize = new Sequelize(db.database, db.user, db.password, {
+//   host: db.host,
 //   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: true
-//   }
 // });
+
+const sequelize = new Sequelize("postgres://taller:mMcLezbgC9kK4mDIJSyg2GkPx6CV4dGx@dpg-ch9c57bhp8u0vhbv0pfg-a.oregon-postgres.render.com/petshop_b264", {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true
+  }
+});
 
 module.exports = {
   sequelize,
